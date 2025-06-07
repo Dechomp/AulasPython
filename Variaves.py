ce30552054d2b0c1f764f3d459c3df445e8ea15a
Dechomp <112034108+Dechomp@users.noreply.github.com>
2025-06-07 09:52:28 -0300

    Add files via upload

---
+++ Variaves.py
@@ -0,0 +1,11 @@
+def main():
+  nome = "Júlio"
+  idade = 140
+  altura = 1.80
+  isMasculino = True
+  print("Ola", nome)
+  print("Idade:", idade)
+  print("Altura:", altura)
+  print("É Masculino:", isMasculino)
+  return 0
+main()
